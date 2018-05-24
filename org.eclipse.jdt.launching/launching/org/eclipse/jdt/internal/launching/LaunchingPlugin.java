9d13bbb449c81a561c28d45e62c9c1595d71d52e
Cedric Chabanois <cchabanois@gmail.com>
2018-05-24 14:08:05 +0200

    Bug 385738 - Shorten the classpath/modulepath if necessary.
    Shorten the classpath and/or modulepath if it gets longer than current
    operating system limits.
    Depending on the java version, os and launch configuration, the
    classpath argument will be replaced by an argument file, a
    classpath-only jar or an env variable.
    The modulepath is replaced by an argument file if necessary.
    If classpath-only jar needs to be used to shorten the classpath, an
    explicit error popup ask if user wants to use classpath-only jar.
    
    Change-Id: Ibcc23b6fa5c5baa05d91aef0f03529fb8e598e08
    Signed-off-by: Cedric Chabanois <cchabanois@gmail.com>

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/LaunchingPlugin.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/LaunchingPlugin.java
@@ -22,6 +22,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
@@ -102,6 +103,11 @@ public class LaunchingPlugin extends Plugin implements DebugOptionsListener, IEc
 
 	public static final String DEBUG_JRE_CONTAINER_FLAG = "org.eclipse.jdt.launching/debug/classpath/jreContainer"; //$NON-NLS-1$
 	public static final String DEBUG_FLAG = "org.eclipse.jdt.launching/debug"; //$NON-NLS-1$
+
+	/**
+	 * list of temp files for the launch (separated by the path separator char)
+	 */
+	public static final String ATTR_LAUNCH_TEMP_FILES = "tempFiles"; //$NON-NLS-1$
 
 	/**
 	 * The {@link DebugTrace} object to print to OSGi tracing
@@ -1178,9 +1184,23 @@ public class LaunchingPlugin extends Plugin implements DebugOptionsListener, IEc
 				Object source = event.getSource();
 				if (source instanceof IDebugTarget || source instanceof IProcess) {
 					ArchiveSourceLocation.closeArchives();
+					IProcess process;
+					if (source instanceof IProcess) {
+						process = (IProcess) source;
+					} else {
+						process = ((IDebugTarget) source).getProcess();
+					}
+					deleteProcessTempFiles(process);
 				}
 			}
 		}
+	}
+
+	private void deleteProcessTempFiles(IProcess process) {
+		if (process.getAttribute(ATTR_LAUNCH_TEMP_FILES) == null) {
+			return;
+		}
+		Arrays.stream(process.getAttribute(ATTR_LAUNCH_TEMP_FILES).split(File.pathSeparator)).map(path -> new File(path)).forEach(file -> file.delete());
 	}
 
 	/**
